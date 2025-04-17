be9167e381181567da655a649e0260f92368a50f
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-04-17 18:44:37 +0200

    Pipe operator

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -26,11 +26,15 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "File: #{filename} does not exists"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File: #{filename} does not exists"
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
