1a4ce89485e8655da038f64e399599af1d7d69c1
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-04-19 18:18:45 +0200

    exercism finish freelancer_rates

--- exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
+++ exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,23 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    8.0 * hourly_rate
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    :ok
+    before_discount - before_discount * discount / 100
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    trunc(
+      ceil(FreelancerRates.apply_discount(FreelancerRates.daily_rate(hourly_rate) * 22, discount))
+    )
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    Float.floor(
+      budget /
+        FreelancerRates.apply_discount(FreelancerRates.daily_rate(hourly_rate), discount),
+      1
+    )
   end
 end
