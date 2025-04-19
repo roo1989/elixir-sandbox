5da8e4062a8139b69cd31e307d2a4b7d1c9b2d31
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-04-19 15:48:52 +0200

    exercism freelancer-rates

---
+++ exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,17 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    8.0 * hourly_rate
+  end
+
+  def apply_discount(before_discount, discount) do
+    :ok
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    # Please implement the monthly_rate/2 function
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    # Please implement the days_in_budget/3 function
+  end
+end
