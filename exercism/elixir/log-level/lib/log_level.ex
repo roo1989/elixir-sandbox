6603b98922af9047ee911e0e9a6222a2b846bda0
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-04-21 13:04:54 +0200

    finish log_level

--- exercism/elixir/log-level/lib/log_level.ex
+++ exercism/elixir/log-level/lib/log_level.ex
@@ -1,9 +1,45 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      legacy? and level in [0, 5] ->
+        :unknown
+
+      level == 0 ->
+        :trace
+
+      level == 1 ->
+        :debug
+
+      level == 2 ->
+        :info
+
+      level == 3 ->
+        :warning
+
+      level == 4 ->
+        :error
+
+      level == 5 ->
+        :fatal
+
+      true ->
+        :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    cond do
+      legacy? and LogLevel.to_label(level, legacy?) == :unknown ->
+        :dev1
+
+      LogLevel.to_label(level, legacy?) in [:error, :fatal] ->
+        :ops
+
+      not legacy? and LogLevel.to_label(level, legacy?) == :unknown ->
+        :dev2
+
+      true ->
+        false
+    end
   end
 end
