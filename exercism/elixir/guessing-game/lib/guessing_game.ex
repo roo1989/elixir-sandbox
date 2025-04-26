c876b2252134d8caedf1911c08101c47f46e67eb
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-04-26 09:26:30 +0200

    exercism(wip): guessing game

--- exercism/elixir/guessing-game/lib/guessing_game.ex
+++ exercism/elixir/guessing-game/lib/guessing_game.ex
@@ -1,4 +1,7 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-  end
+  def compare(_, nil), do: "Make a guess"
+  def compare(nil, _), do: "Make a guess"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too high"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too low"
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
 end
