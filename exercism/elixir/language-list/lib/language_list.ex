ae6802056440feb9c0630624c5c0282f2b609062
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-05-20 14:14:05 +0000

    wip

--- exercism/elixir/language-list/lib/language_list.ex
+++ exercism/elixir/language-list/lib/language_list.ex
@@ -8,22 +8,18 @@ defmodule LanguageList do
   end
 
   def remove(list) do
-    # Please implement the remove/1 function
     List.delete_at(list, 0)
   end
 
   def first(list) do
-    # Please implement the first/1 function
     List.first(list, [])
   end
 
   def count(list) do
-    # Please implement the count/1 function
     length(list)
   end
 
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
     "Elixir" in list
   end
 end
