5bef49f87ed9b7c70de17d3c6fbfc950befeb0c0
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-05-23 15:10:23 +0000

    tuple examples

--- data-types/lib/tuples.ex
+++ data-types/lib/tuples.ex
@@ -1,6 +1,29 @@
 defmodule Tuples do
+  require Logger
+
   def main() do
     my_tuple = {1, 2, 3, 5}
-    Enum.all?(my_tuple)
+
+    # Access an element of a tuple
+    first_elem = elem(my_tuple, 1)
+    Logger.info(first_elem)
+  end
+
+  @doc """
+  A very simple function to see how to read tuples in action.
+  """
+  def access_file() do
+    # Try to create a file
+    file = File.write("./test.txt", "Hello, World!")
+
+    case File.read(file) do
+      {:ok, data} ->
+        Logger.info("File content: #{data}")
+        :ok
+
+      {:error, reason} ->
+        Logger.error("Some error happened: #{reason}")
+        :error
+    end
   end
 end
