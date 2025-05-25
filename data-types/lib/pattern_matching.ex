9338663b975d488c501d4b020fd6de89a3722ef7
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-05-25 16:39:55 +0000

    pattern matching

---
+++ data-types/lib/pattern_matching.ex
@@ -0,0 +1,37 @@
+defmodule PatternMatching do
+  def simple_example do
+    my_variable = 10
+    IO.puts(my_variable)
+
+    # Match it to something else
+    my_variable = 20
+    IO.puts(my_variable)
+
+    my_string = "Hello, world!"
+    IO.puts(my_string)
+  end
+
+  def list_example do
+    [head | tail] = [1, 2, 3, 4]
+    IO.puts(head)
+    IO.puts(tail)
+    [age, name] = [24, "Ragnar"]
+    IO.puts(age)
+    IO.puts(name)
+  end
+
+  def tuple_example do
+    {status, message} = {:ok, "200 OK"}
+    IO.puts(status)
+    IO.puts(message)
+  end
+
+  def map_example do
+    %{key: value} = %{key: "Age", value: 24}
+    IO.puts(value)
+  end
+
+  def ignore_example do
+    _name = "Ragnar"
+  end
+end
